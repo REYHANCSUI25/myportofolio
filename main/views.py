3d08582e3404f954fc07264583dfa64879ca6b33
REYHANCSUI25 <muhammad.reyhan53@ui.ac.id>
2026-09-09 22:02:16 +0700

    Wire up views, templates, and routing for Experience page

---
+++ main/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+from main.models import Experience
+# Create your views here.
+
+def show_main(request):
+    context = {
+        "name": "Muhammad Reyhan Attarizky",
+        "npm": "2506637086",
+        "study_program": "S1 Ilmu Komputer KKI",
+        "bio": "Sophomore CS student barely surviving at Universitas Indonesia with delusions of working in artificial intelligence someday.",
+    }
+    return render(request, "index.html", context)
+
+
+def show_experience(request):
+    context = {
+        "name": "Muhammad Reyhan Attarizky",
+        "experience_list": Experience.objects.all(),
+    }
+    return render(request, "experience.html", context)
